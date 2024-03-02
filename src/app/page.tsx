26ddbf41288d1b5c6b186ad775acebffce71b4fb
zonejay <zyj1179035793@gmail.com>
2024-03-02 16:52:41 +0800

    feat: add google site verification

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import Head from 'next/head'
 import {useState} from 'react'
 enum Minions {
   'R' = 'R',
@@ -60,58 +61,66 @@ export default function Home() {
   }
 
   return (
-    <div className="flex justify-center">
-      <div className="max-w-screen-md mx-auto">
-        <h1 className="text-3xl font-bold text-center mt-8 mb-4">抽卡模拟</h1>
-        <div className="p-4 bg-gray-100 shadow-md">
-          <div className="flex justify-center mt-4">
-            <button
-              onClick={singleDraw}
-              className="mr-4 px-4 py-2 bg-blue-500 text-white font-semibold rounded hover:bg-blue-600"
-            >
-              单抽
-            </button>
-            <button
-              onClick={multiDraw}
-              className="mr-4 px-4 py-2 bg-blue-500 text-white font-semibold rounded hover:bg-blue-600"
-            >
-              十连
-            </button>
-            <button
-              onClick={clearResults}
-              className="px-4 py-2 bg-orange-500 text-white font-semibold rounded hover:bg-orange-600"
-            >
-              清空
-            </button>
-          </div>
-          <div className="text-center mt-4">
-            <p className="text-sm">R阶78.75%，SR阶20%，SSR阶1%，SP阶0.25%</p>
-          </div>
-          <div className="mt-8">
-            <div className="text-center">
-              <h2 className="text-xl font-semibold">抽卡结果</h2>
+    <>
+      <Head>
+        <title>抽卡模拟器</title>
+        <meta name="description" content="阴阳师抽卡模拟" />
+        <link rel="canonical" href="https://draw-card-eight.vercel.app" />
+        {/* 添加其他 SEO 相关的 meta 标签 */}
+      </Head>
+      <div className="flex justify-center">
+        <div className="max-w-screen-md mx-auto">
+          <h1 className="text-3xl font-bold text-center mt-8 mb-4">抽卡模拟</h1>
+          <div className="p-4 bg-gray-100 shadow-md">
+            <div className="flex justify-center mt-4">
+              <button
+                onClick={singleDraw}
+                className="mr-4 px-4 py-2 bg-blue-500 text-white font-semibold rounded hover:bg-blue-600"
+              >
+                单抽
+              </button>
+              <button
+                onClick={multiDraw}
+                className="mr-4 px-4 py-2 bg-blue-500 text-white font-semibold rounded hover:bg-blue-600"
+              >
+                十连
+              </button>
+              <button
+                onClick={clearResults}
+                className="px-4 py-2 bg-orange-500 text-white font-semibold rounded hover:bg-orange-600"
+              >
+                清空
+              </button>
             </div>
-            <div className="mt-4 grid grid-cols-2 gap-x-4">
-              <div>
-                <p className="font-semibold">SP:</p>
-                <p>数量: {resultCounts.SP}</p>
+            <div className="text-center mt-4">
+              <p className="text-sm">R阶78.75%，SR阶20%，SSR阶1%，SP阶0.25%</p>
+            </div>
+            <div className="mt-8">
+              <div className="text-center">
+                <h2 className="text-xl font-semibold">抽卡结果</h2>
               </div>
-              <div>
-                <p className="font-semibold">SSR:</p>
-                <p>数量: {resultCounts.SSR}</p>
-              </div>
-              <div>
-                <p className="font-semibold">SR:</p>
-                <p>数量: {resultCounts.SR}</p>
-              </div>
-              <div>
-                <p className="font-semibold">R:</p>
-                <p>数量: {resultCounts.R}</p>
+              <div className="mt-4 grid grid-cols-2 gap-x-4">
+                <div>
+                  <p className="font-semibold">SP:</p>
+                  <p>数量: {resultCounts.SP}</p>
+                </div>
+                <div>
+                  <p className="font-semibold">SSR:</p>
+                  <p>数量: {resultCounts.SSR}</p>
+                </div>
+                <div>
+                  <p className="font-semibold">SR:</p>
+                  <p>数量: {resultCounts.SR}</p>
+                </div>
+                <div>
+                  <p className="font-semibold">R:</p>
+                  <p>数量: {resultCounts.R}</p>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
